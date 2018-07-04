9a4fe39b6661dbe848764591ff081901bccbaeee
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-04 19:56:40 +0530

    Max char solution!

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Solution
+const maxChar = (str) => {
+  const charMap = {};
+
+  for (const char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  let solution;
+  let maxValue = 0;
+
+  for (const char in charMap) {
+    if (charMap[char] > maxValue) {
+      maxValue = charMap[char];
+      solution = char;
+    }
+  }
+
+  return solution;
+};
 
 module.exports = maxChar;
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,14 +1,18 @@
 const maxChar = require('./index');
 
 test('maxChar function exists', () => {
-  expect(typeof maxChar).toEqual('function');
+  expect(typeof maxChar)
+    .toEqual('function');
 });
 
 test('Finds the most frequently used char', () => {
-  expect(maxChar('a')).toEqual('a');
-  expect(maxChar('abcdefghijklmnaaaaa')).toEqual('a');
+  expect(maxChar('a'))
+    .toEqual('a');
+  expect(maxChar('abcdefghijklmnaaaaa'))
+    .toEqual('a');
 });
 
 test('Works with numbers in the string', () => {
-  expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
+  expect(maxChar('ab1c1d1e1f1g1'))
+    .toEqual('1');
 });
