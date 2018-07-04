fe4368b2178fb8e3ded5e7b53f7645b0df7c588a
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-04 20:42:03 +0530

    Eslint update and readme deletion!

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,11 @@ module.exports = {
   rules: {
     // Personal Preferences below ... proffessionally may change 
     'arrow-body-style': 0, // Love Arrow Functions but not a fan of single liner functions!
+    'arrow-parens': 0,
     'func-names': 0, // Mainly for old school constructor functions
     'no-underscore-dangle': 0, // Preference for using Private Properties through ES6 Symbols in an Object
     'symbol-description': 0, // Preference for using Private Properties through ES6 Symbols in an Object
+    'brace-style': 0,
     'no-unused-vars': 0,
     'no-undef': 0,
     'no-trailing-spaces': 0,
