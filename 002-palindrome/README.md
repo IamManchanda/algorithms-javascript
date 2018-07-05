db119334311bc2b126da5c94574135ff6dc4ae2f
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-05 19:01:16 +0530

    Add readme for Palindrome!

---
+++ 002-palindrome/README.md
@@ -0,0 +1,12 @@
+# Palindromes
+
+## Directions
+
+Given a string, return true if the string is a palindrome or false if it is not.  Palindromes are strings that form the same word if it is reversed. **Do** include spaces and punctuation in determining if the string is a palindrome.
+
+## Examples
+
+```javascript
+palindrome("abba") === true
+palindrome("abcdefg") === false
+```
--- 002-palindrome/index.js
+++ 002-palindrome/index.js
@@ -1,12 +1,3 @@
-// --- Directions
-// Given a string, return true if the string is a palindrome
-// or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. *Do* include spaces
-// and punctuation in determining if the string is a palindrome.
-// --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
-
 // Solution 1
 const palindrome1 = (str) => str === str.split('').reverse().join('');
 
