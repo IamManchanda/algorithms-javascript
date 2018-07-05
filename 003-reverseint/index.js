3168b0e46f6af6f03d1546d5e715ae955887ebc7
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-05 19:06:46 +0530

    Reverse Integer!

--- 003-reverseint/index.js
+++ 003-reverseint/index.js
@@ -1,13 +1,3 @@
-// --- Directions
-// Given an integer, return an integer that is the reverse
-// ordering of numbers.
-// --- Examples
-//   reverseInt(15) === 51
-//   reverseInt(981) === 189
-//   reverseInt(500) === 5
-//   reverseInt(-15) === -51
-//   reverseInt(-90) === -9
-
 // Solution
 const reverseInt = (num) => Math.sign(num) * parseInt(num.toString().split('').reverse().join(''), 10);
 
