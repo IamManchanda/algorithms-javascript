8327032a95e25e2c83ddc913013bf29ad3d012f5
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-05 19:17:38 +0530

    Array Chunking!

--- 006-chunk/index.js
+++ 006-chunk/index.js
@@ -1,13 +1,3 @@
-// --- Directions
-// Given an array and chunk size, divide the array into many subarrays
-// where each subarray is of length size
-// --- Examples
-// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
-// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
-// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
-// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
-// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-
 // Solution 1
 const chunk1 = (array, size) => {
   const chunked = [];
