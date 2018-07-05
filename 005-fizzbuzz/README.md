3855cc042e746d6dac39bba4d1c1773f3ed88e94
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-05 19:15:19 +0530

    Add readme for fizzbuzz!

---
+++ 005-fizzbuzz/README.md
@@ -0,0 +1,26 @@
+# The Classic FizzBuzz!
+
+## Directions
+
+Write a program that console logs the numbers from 1 to n. But for multiples of three print “fizz” instead of the number and for the multiples of five print “buzz”. For numbers which are multiples of both three and five print “fizzbuzz”.
+
+## Examples
+
+```javascript
+> fizzBuzz(15);
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+```
--- 005-fizzbuzz/index.js
+++ 005-fizzbuzz/index.js
@@ -1,17 +1,3 @@
-// --- Directions
-// Write a program that console logs the numbers
-// from 1 to n. But for multiples of three print
-// “fizz” instead of the number and for the multiples
-// of five print “buzz”. For numbers which are multiples
-// of both three and five print “fizzbuzz”.
-// --- Example
-//   fizzBuzz(5);
-//   1
-//   2
-//   fizz
-//   4
-//   buzz
-
 // Solution 1
 const fizzBuzz1 = (n) => {
   for (let i = 1; i <= n; i += 1) {
