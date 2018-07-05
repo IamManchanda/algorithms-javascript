a43e614f9a93820b5b7c6dfbc9fb1065bbf8cab4
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-05 19:21:25 +0530

    Readme update for Anagrams

---
+++ 007-anagrams/README.md
@@ -0,0 +1,13 @@
+# Anagrams
+
+## Directions
+
+Check to see if two provided strings are anagrams of eachother. One string is an anagram of another if it uses the same characters in the same quantity. Only consider characters, not spaces or punctuation. Consider capital letters to be the same as lower case
+
+## Examples
+
+```javascript
+anagrams('rail safety', 'fairy tales') === True
+anagrams('RAIL! SAFETY!', 'fairy tales') === True
+anagrams('Hi there', 'Bye there') === False
+```
--- 007-anagrams/index.js
+++ 007-anagrams/index.js
@@ -1,13 +1,3 @@
-// --- Directions
-// Check to see if two provided strings are anagrams of eachother.
-// One string is an anagram of another if it uses the same characters
-// in the same quantity. Only consider characters, not spaces
-// or punctuation.  Consider capital letters to be the same as lower case
-// --- Examples
-//   anagrams('rail safety', 'fairy tales') --> True
-//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
-//   anagrams('Hi there', 'Bye there') --> False
-
 // Helper Function for both solutions
 const cleanString = (str) => str.replace(/[^\w]/g, '').toLowerCase();
 
