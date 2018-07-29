5b6a6c42c0616036d5473f542736e55057a71d5e
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-29 22:27:02 +0530

    First pass on linked lists!

---
+++ 019-linkedlist/Readme.md
@@ -0,0 +1,5 @@
+# Linked List
+
+## Directions
+
+Implement classes Node and Linked Lists. See [**directions**](./directions.html) document (View the HTML page from a browser.)
---
+++ 019-linkedlist/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter += 1;
+      node = node.next;
+    }
+    return counter;
+  }
+}
+
+module.exports = { Node, LinkedList };
