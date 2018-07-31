90bfb1181dff4e96af08785f936dde352ae1afe2
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-31 16:43:14 +0530

    Working pass on midpoint of linked list!

--- 020-midpoint-linkedlist/index.js
+++ 020-midpoint-linkedlist/index.js
@@ -1,5 +1,12 @@
 // Solution
 const midpoint = (list) => {
+  let advBy1 = list.getFirst();
+  let advBy2 = list.getFirst();
+  while (advBy2.next && advBy2.next.next) {
+    advBy1 = advBy1.next;
+    advBy2 = advBy2.next.next;
+  }
+  return advBy1;
 };
 
 module.exports = midpoint;
