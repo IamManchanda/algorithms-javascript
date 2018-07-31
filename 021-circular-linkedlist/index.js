7e463759d6a7e2a523ded05645893aaf7302cd30
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-31 17:34:31 +0530

    Working pass on circular linked list!

--- 021-circular-linkedlist/index.js
+++ 021-circular-linkedlist/index.js
@@ -1,5 +1,13 @@
 // Solution
 const circular = (list) => {
+  let advBy1 = list.getFirst();
+  let advBy2 = list.getFirst();
+  while (advBy2.next && advBy2.next.next) {
+    advBy1 = advBy1.next;
+    advBy2 = advBy2.next.next;
+    if (advBy1 === advBy2) return true;
+  }
+  return false;
 };
 
 module.exports = circular;
