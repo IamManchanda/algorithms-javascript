040fff365aa949ac7cf83cebadc5420babf0dc9d
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-08-08 13:49:20 +0530

    Working pass on from last function!

--- 022-fromlast-linkedlist/index.js
+++ 022-fromlast-linkedlist/index.js
@@ -1,6 +1,16 @@
 // Solution
 const fromLast = (list, n) => {
-
+  let slowIncrement = list.getFirst();
+  let fastIncrement = list.getFirst();
+  while (n > 0) {
+    fastIncrement = fastIncrement.next;
+    n -= 1;
+  }
+  while (fastIncrement.next) {
+    slowIncrement = slowIncrement.next;
+    fastIncrement = fastIncrement.next;
+  }
+  return slowIncrement;
 };
 
 module.exports = fromLast;
