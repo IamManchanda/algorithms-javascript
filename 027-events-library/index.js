98bada0c2080a708a95e76a86ffe1725c5fb2bbf
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-08-15 14:57:02 +0530

    Working pass on eventing system!

--- 027-events-library/index.js
+++ 027-events-library/index.js
@@ -1,13 +1,20 @@
 // Solution
-
 class Events {
+  constructor() {
+    this.events = {};
+  }
+
   on(eventName, callback) {
+    if (this.events[eventName]) this.events[eventName].push(callback);
+    else this.events[eventName] = [callback];
   }
 
   trigger(eventName) {
+    if (this.events[eventName]) for (let cb of this.events[eventName]) cb();
   }
 
   off(eventName) {
+    delete this.events[eventName];
   }
 }
 
